1e7d20d7171c11fcf2d474d3f60ddc76b8e5a4c0
HDB-DCO <dcohdb@gmail.com>
2024-07-29 11:27:41 +0800

    Modified and added .env

--- src/config/authConfig.js
+++ src/config/authConfig.js
@@ -3,7 +3,7 @@ export const msalConfig = {
     auth: {
         clientId: 'b11b9455-88ff-4f2f-92fa-0caf915e91d8',
         authority: 'https://login.microsoftonline.com/common',
-        redirectUri: getCurrentUrl()
+        redirectUri: process.env.REACT_APP_REDIRECT_URI
     },
     cache: {
         cacheLocation: 'localStorage',
@@ -14,7 +14,3 @@ export const msalConfig = {
 export const loginRequest = {
     scopes: ['User.Read']
 };
-
-const getCurrentUrl = () => {
-    return `${window.location.origin}/auth/callback`;
-  };
