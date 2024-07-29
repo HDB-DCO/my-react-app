354d6cd2dd65efe856b54640866a23381dd803dc
HDB-DCO <dcohdb@gmail.com>
2024-07-29 14:35:47 +0800

    reference from Microsoft

--- src/config/authConfig.js
+++ src/config/authConfig.js
@@ -1,16 +1,49 @@
-// authConfig.js
+import { LogLevel } from "@azure/msal-browser";
+
+
 export const msalConfig = {
     auth: {
-        clientId: 'b11b9455-88ff-4f2f-92fa-0caf915e91d8',
-        authority: 'https://login.microsoftonline.com/common',
+        clientId: "b11b9455-88ff-4f2f-92fa-0caf915e91d8",
+        authority: "https://login.microsoftonline.com/common",
         redirectUri: process.env.REACT_APP_REDIRECT_URI
     },
     cache: {
-        cacheLocation: 'localStorage',
-        storeAuthStateInCookie: true
+        cacheLocation: "sessionStorage",
+        storeAuthStateInCookie: false,
+    },
+    system: {	
+        loggerOptions: {	
+            loggerCallback: (level, message, containsPii) => {	
+                if (containsPii) {		
+                    return;		
+                }		
+                switch (level) {
+                    case LogLevel.Error:
+                        console.error(message);
+                        return;
+                    case LogLevel.Info:
+                        console.info(message);
+                        return;
+                    case LogLevel.Verbose:
+                        console.debug(message);
+                        return;
+                    case LogLevel.Warning:
+                        console.warn(message);
+                        return;
+                    default:
+                        return;
+                }	
+            }	
+        }	
     }
 };
 
+
 export const loginRequest = {
-    scopes: ['User.Read']
+    scopes: ["User.Read"]
+};
+
+
+export const graphConfig = {
+    graphMeEndpoint: "https://graph.microsoft.com/v1.0/me" //e.g. https://graph.microsoft.com/v1.0/me
 };
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import SignInButton from '../components/SignInButton';
 
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from '@azure/msal-react';
+import { loginRequest } from '../config/authConfig';
+import { callMsGraph } from '../graph';
+import { ProfileData } from '../components/ProfileData';
+import { PageLayout } from '../components/PageLayout';
+import Button from 'react-bootstrap/Button';
+
+/**
+ * Renders information about the signed-in user or a button to retrieve data about the user
+ */
+const ProfileContent = () => {
+    const { instance, accounts } = useMsal();
+    const [graphData, setGraphData] = useState(null);
+
+    function RequestProfileData() {
+        // Silently acquires an access token which is then attached to a request for MS Graph data
+        instance
+            .acquireTokenSilent({
+                ...loginRequest,
+                account: accounts[0],
+            })
+            .then((response) => {
+                callMsGraph(response.accessToken).then((response) => setGraphData(response));
+            });
+    }
+
+    return (
+        <>
+            <h5 className="card-title">Welcome {accounts[0].name}</h5>
+            {graphData ? (
+                <ProfileData graphData={graphData} />
+            ) : (
+                <Button variant="secondary" onClick={RequestProfileData}>
+                    Request Profile Information
+                </Button>
+            )}
+        </>
+    );
+};
+
+/**
+ * If a user is authenticated the ProfileContent component above is rendered. Otherwise a message indicating a user is not authenticated is rendered.
+ */
+const MainContent = () => {
+    return (
+        <div className="App">
+            <AuthenticatedTemplate>
+                <ProfileContent />
+            </AuthenticatedTemplate>
+
+            <UnauthenticatedTemplate>
+                <h5 className="card-title">Please sign-in to see your profile information.</h5>
+            </UnauthenticatedTemplate>
+        </div>
+    );
+};
 
 
 const Homepage = () => {
@@ -45,9 +101,13 @@ const Homepage = () => {
     // Render the data once it's loaded
     return (
         <div>
+        <h1>Environment Variable Value:</h1>
+        <p>{process.env.REACT_APP_REDIRECT_URI}</p>
         <h1>Fetched Data:</h1>
         <pre>{JSON.stringify(data, null, 2)}</pre>
-        <SignInButton />
+        <PageLayout>
+            <MainContent />
+        </PageLayout>
         </div>
     );    
 };
