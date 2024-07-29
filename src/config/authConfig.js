497efba76847ad1d81bc2e31df6f187403fadc63
HDB-DCO <dcohdb@gmail.com>
2024-07-29 11:18:38 +0800

    Modified currentUrl()

--- src/config/authConfig.js
+++ src/config/authConfig.js
@@ -3,14 +3,18 @@ export const msalConfig = {
     auth: {
         clientId: 'b11b9455-88ff-4f2f-92fa-0caf915e91d8',
         authority: 'https://login.microsoftonline.com/common',
-        redirectUri: 'http://localhost:3000/auth/callback'
+        redirectUri: getCurrentUrl()
     },
     cache: {
         cacheLocation: 'localStorage',
-        storeAuthStateInCookie: false
+        storeAuthStateInCookie: true
     }
 };
 
 export const loginRequest = {
     scopes: ['User.Read']
 };
+
+const getCurrentUrl = () => {
+    return `${window.location.origin}/auth/callback`;
+  };
