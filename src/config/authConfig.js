66b800ba3a3d8d19266106804b58218a6f035201
HDB-DCO <dcohdb@gmail.com>
2024-07-29 14:46:47 +0800

    modified for window.location

--- src/config/authConfig.js
+++ src/config/authConfig.js
@@ -5,7 +5,7 @@ export const msalConfig = {
     auth: {
         clientId: "b11b9455-88ff-4f2f-92fa-0caf915e91d8",
         authority: "https://login.microsoftonline.com/common",
-        redirectUri: process.env.REACT_APP_REDIRECT_URI
+        redirectUri: window.location.origin //process.env.REACT_APP_REDIRECT_URI
     },
     cache: {
         cacheLocation: "sessionStorage",
