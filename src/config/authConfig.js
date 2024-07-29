e0da4ac255993f2f80d664cd11d83d3626285af6
HDB-DCO <dcohdb@gmail.com>
2024-07-29 10:43:30 +0800

    Modified application Id

--- src/config/authConfig.js
+++ src/config/authConfig.js
@@ -1,8 +1,8 @@
 // authConfig.js
 export const msalConfig = {
     auth: {
-        clientId: '08caf53e-c8f2-4833-b795-5e873e0b7d75',
-        authority: 'https://login.microsoftonline.com/dcohdbgmail.onmicrosoft.com',
+        clientId: 'b11b9455-88ff-4f2f-92fa-0caf915e91d8',
+        authority: 'https://login.microsoftonline.com/common',
         redirectUri: 'http://localhost:3000/auth/callback'
     },
     cache: {
